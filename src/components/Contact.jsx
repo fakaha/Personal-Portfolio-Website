df6daf431b07507b2a747dae7f84467734deb1bf
fakaha <lunastra34@gmail.com>
2024-05-21 11:18:21 +0700

    feat: change image social media

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import ig from '../assets/5340248_ig_instagram_photo_icon.svg'
-import linkedin from '../assets/104493_linkedin_icon.svg'
-import github from '../assets/211904_social_github_icon.svg'
+import ig from '../assets/icon-instagram.svg'
+import linkedin from '../assets/icon-linkedin.svg'
+import github from '../assets/icons8-github.svg'
 
 const Contact = () => {
   const goto = (socialMedia) =>{
@@ -11,9 +11,9 @@ const Contact = () => {
     <section id='contact' className='contact bg-primary d-flex flex-column justify-center align-center'>
         <h2 className='text-light'>My Social Media</h2>
         <div className='social-medias d-flex'>
-            <img src={ig} alt="" onClick={() => goto('https://www.instagram.com/zulfa_fakaha')}/>
-            <img src={linkedin} alt="" onClick={() => goto('https://www.linkedin.com/in/zulfa-fakaha')}/>
-            <img src={github} alt="" onClick={() => goto('https://github.com/fakaha')}/>
+            <img src={ig} alt="instagram" onClick={() => goto('https://www.instagram.com/zulfa_fakaha')}/>
+            <img src={linkedin} alt="linkedin" onClick={() => goto('https://www.linkedin.com/in/zulfa-fakaha')}/>
+            <img src={github} alt="github" onClick={() => goto('https://github.com/fakaha')}/>
         </div>
     </section>
   )
