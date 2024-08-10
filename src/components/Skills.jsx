b362906d1f5fc69f1ed3300682d05fe8523a1df6
fakaha <lunastra34@gmail.com>
2024-08-10 22:34:03 +0700

    style: add gap in skills mobile view

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
   return (
     <section id='skills' className='skills flex flex-col justify-center p-5'>
         <h2 className='text-[#fff] text-2xl'>My Skills</h2>
-        <div className='flex justify-center mt-5 gap-x-8 text-[#fff] flex-wrap'>
+        <div className='flex justify-center mt-5 gap-x-8 gap-y-6 text-[#fff] flex-wrap'>
             <div className='w-[90px] text-center flex flex-col'><p>Experienced</p><img src={HTMLIcon} alt="HTML" /><p>HTML</p></div>
             <div className='w-[90px] text-center flex flex-col'><p>Experienced</p><img src={CSSIcon} alt="CSS" /><p>CSS</p></div>
             <div className='w-[90px] text-center flex flex-col'><p>Intermediate</p><img src={JSIcon} alt="Javascript" /><p>Javascript</p></div>
