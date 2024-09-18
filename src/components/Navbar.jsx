656c0656e8bbc1d8f00ecae51471123900a0f6bc
fakaha <lunastra34@gmail.com>
2024-09-18 17:00:48 +0700

    add zindex nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { Button } from "./Button/Button";
 const Navbar = () => {
 
   return (
-    <nav className="flex justify-between px-8 py-7 items-center fixed top-0 right-0 left-0 bg-[#fff] lg:mx-12 mx-8 mt-7 w-max-md rounded-3xl">
+    <nav className="flex justify-between px-8 py-7 items-center fixed top-0 right-0 left-0 bg-[#fff] lg:mx-12 mx-8 mt-7 w-max-md rounded-3xl z-10">
       <h1 onClick={() => {window.scrollTo({top: 0, left: 0, behavior: 'smooth'})}} className="text-xl lg:text-3xl hover:cursor-pointer">ZFakaha</h1>
       <div className="flex gap-2 lg:gap-5 items-center">
         <Link className="hover:text-[#007bff]" smooth to="#home">Home</Link>
