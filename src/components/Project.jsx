a45075638810f02506ea75d1f2ce62ebc4718895
fakaha <lunastra34@gmail.com>
2024-11-02 20:57:15 +0700

    feat: add 1 project e-commerce API and add margin top on the description project and tech stack project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import project1 from '../assets/todoProject.webp'
-import project2 from '../assets/movieProject.webp'
-import project3 from '../assets/courseProject.webp'
-import project4 from '../assets/project4.webp'
-import project5 from '../assets/emporium-z.webp'
+import React from "react";
+import project1 from "../assets/todoProject.webp";
+import project2 from "../assets/movieProject.webp";
+import project3 from "../assets/courseProject.webp";
+import project4 from "../assets/project4.webp";
+import project5 from "../assets/emporium-z.webp";
+import projectapi1 from "../assets/projectapi1.webp";
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 
 const Project = () => {
-  const goto = (project) =>{
-    window.open(project)
-  }
+  const goto = (project) => {
+    window.open(project);
+  };
   AOS.init();
   AOS.init({
     // Global settings:
@@ -45,41 +46,49 @@ const Project = () => {
           className="project-content lg:w-[30%] w-full"
         >
           <img
-            className="w-80"
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
             src={project1}
             alt=""
             onClick={() => goto("https://local-todo-apps-zul.vercel.app/")}
           />
-          <p>
+          <p className="mt-2">
             Todo List website that uses local storage to save data without a
             database.
           </p>
+          <b className="text-[#007bff] mt-2 block">
+            Bootstrap, Javascript and React JS
+          </b>
         </div>
         <div
           data-aos="zoom-in-down"
           className="project-content lg:w-[30%] w-full"
         >
           <img
-            className="w-80"
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
             src={project2}
             alt=""
             onClick={() =>
               goto("https://challenge-06-zulfa-fakaha.vercel.app/")
             }
           />
-          <p>Movie Website that displays several movies using a public API.</p>
+          <p className="mt-2">
+            Movie Website that displays several movies using a public API.
+          </p>
+          <b className="text-[#007bff] mt-2 block">
+            Bootstrap, Javascript and React JS
+          </b>
         </div>
         <div
           data-aos="zoom-in-down"
           className="project-content lg:w-[30%] w-full"
         >
           <img
-            className="w-80"
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
             src={project3}
             alt=""
             onClick={() => goto("https://fpbinar-kel7.vercel.app/")}
           />
-          <p>
+          <p className="mt-2">
             Online Course website that features buying and watching for its
             users using a custom API.
             <br />
@@ -88,40 +97,68 @@ const Project = () => {
             <span className="text-secondary">Front End</span> for the final
             project)
           </p>
+          <b className="text-[#007bff] mt-2 block">
+            Bootstrap, Javascript and React JS
+          </b>
         </div>
         <div
           data-aos="zoom-in-down"
           className="project-content lg:w-[30%] w-full"
         >
           <img
-            className="w-80"
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
             src={project4}
             alt=""
             onClick={() => goto("https://quranku-tau.vercel.app/")}
           />
-          <p>
+          <p className="mt-2">
             Quran website that features reading for its users using a API
             public.
           </p>
+          <b className="text-[#007bff] mt-2 block">
+            Tailwind CSS, Javascript and React JS
+          </b>
         </div>
         <div
           data-aos="zoom-in-down"
           className="project-content lg:w-[30%] w-full"
         >
           <img
-            className="w-80"
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
             src={project5}
             alt=""
             onClick={() => goto("https://emporium-z.vercel.app/")}
           />
-          <p>
+          <p className="mt-2">
             Store website that features login, register, and adding items to
             cart for its users using a public API.
           </p>
+          <b className="text-[#007bff] mt-2 block">
+            Tailwind CSS, Javascript, React JS and Redux
+          </b>
+        </div>
+        <div
+          data-aos="zoom-in-down"
+          className="project-content lg:w-[30%] w-full"
+        >
+          <img
+            className="w-80 hover:opacity-75 hover:scale-105 duration-300"
+            src={projectapi1}
+            alt=""
+            onClick={() => goto("https://sanberbe60-zul.vercel.app/docs/#/")}
+          />
+          <p className="mt-2">
+            A simple e-commerce Web API with features for user registration,
+            login, managing products, managing product categories, and managing
+            product orders.
+          </p>
+          <b className="text-[#007bff] mt-2 block">
+            Typescript, Node JS, Express JS, MongoDB and Mongoose
+          </b>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Project
+export default Project;
